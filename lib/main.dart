1006ea6f434eb2ef131854effbbe728303695ba3
Muhammed Kara <muhammedkara1988@gmail.com>
2024-12-16 15:53:16 +0200

    Title Dinamik

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TMDB Movie App',
+      title: 'TMDB Movie App', // Dinamik yapılacak
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
