f76d45450e010db6ed9a5f942481f1b4e3c06d36
Ziyaaddin Yaramis <ziyaaddinyaramis@gmail.com>
2024-12-16 15:35:11 +0200

    l10n Dil Dosyalari

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tmdb_movie_app/screens/home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+
 Future<void> main() async {
   await dotenv.load(fileName: "lib/assets/.env");
   runApp(MyApp());
